b614a54950acf8c04e713f3f60c0ceb4515539b8
Saurabh Satralkar <saurabh.satralkar7@gmail.com>
2022-09-22 09:48:06 +0200

    The logic is almost built for to-do list using EJS

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var newItems = ["Buy food", "Cook food", "Eat food"];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Home route
+app.get("/", function(req, res){
+   // res.send("Hello");
+
+    var today  = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-UK", options);
+    res.render("list", {kindOFDay: day, newListItems: newItems});
+
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+}); 
+
+//Port route
+app.post("/", function(req, res){
+    var newItem  = req.body.newItem;
+    newItems.push(newItem);
+    res.redirect("/");
+});
